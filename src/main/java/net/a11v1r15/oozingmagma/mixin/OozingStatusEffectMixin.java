de077db748a783c7f2bce0bbf1866e71d76fcd92
Álvaro Carvalho <a11v1r15@Rocketmail.com>
2024-03-29 13:55:17 -0300

    :bug: Fix issue #1

--- src/main/java/net/a11v1r15/oozingmagma/mixin/OozingStatusEffectMixin.java
+++ src/main/java/net/a11v1r15/oozingmagma/mixin/OozingStatusEffectMixin.java
@@ -1,20 +1,15 @@
 package net.a11v1r15.oozingmagma.mixin;
 
-import net.minecraft.world.World;
-
-import java.util.List;
-
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.ModifyVariable;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-import net.minecraft.entity.Entity;
-import net.minecraft.entity.effect.OozingStatusEffect;
+
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.effect.StatusEffect;
+import net.minecraft.entity.effect.OozingStatusEffect;
 import net.minecraft.entity.effect.StatusEffectCategory;
 import net.minecraft.entity.mob.SlimeEntity;
-import net.minecraft.util.math.Box;
+import net.minecraft.world.World;
 
 @Mixin(OozingStatusEffect.class)
 public class OozingStatusEffectMixin
@@ -28,7 +23,7 @@ extends StatusEffect {
         method = "spawnSlime(Lnet/minecraft/world/World;DDD)V",
         at = @At(value = "STORE"), ordinal = 0
         )
-    private SlimeEntity oozingMagma$changeSlimeToMagmaCube(World world, SlimeEntity slime) {
+    private SlimeEntity oozingMagma$changeSlimeToMagmaCube(SlimeEntity slime, World world) {
 		if(world.getDimension().ultrawarm())
 			slime = EntityType.MAGMA_CUBE.create(world);
         return slime;
